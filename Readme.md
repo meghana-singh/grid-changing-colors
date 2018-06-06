e852cd51ccd22827fd5b730080e8772b6b2eb9b5
Meghana Singh <meghana.singh@bloc.io>
2018-06-06 10:00:37 -0700

    Cleaned up the code

--- Readme.md
+++ Readme.md
@@ -1,8 +1,8 @@
 ## Changing Grid Colors
 It's a web page that has a 4x4 grid, with a border around it. The grid is horizontally centered in the web page. Every 250 milliseconds, one of the eligible squares randomly selected change colors. Squares are eligible for a color change if its color has not changed in the last two seconds.
 
-## Run the application
-To view the changing color grids, open the index.html in your browser.
+## To view the webpage
+To view the changing color grids, clone the repo and open the index.html in your browser.
 
 ## Future Improvements:
 The size of the grid and the time periods can be changed in the grid.js file by changing the constants defined at the top of the file. But the css file is not automated to change based on the grid size, css has to be modified manually to take in the grid size changes. I have to improve the CSS.
--- grid.js
+++ grid.js
@@ -8,40 +8,41 @@ var gridHash = {};
 var gridSize = gridX * gridY;
 var gridColor = 'red';
 
-createGridHash();
-createHTMLGrid();
-setInterval(changeGridColors, timeInterval);
+init();
+
+function init() {
+  createGridHash();
+  createHTMLGrid();
+  setInterval(changeGridColors, timeInterval);
+}
 
 function createGridHash() {
-    for(let gridId=0; gridId<gridSize; gridId++) {
-        gridHash[gridId] = {};
-        gridHash[gridId]['red'] = null;
-        gridHash[gridId]['blue'] = null;
-    }
-    console.log('gridHash: ', gridHash);
+  for(let gridId=0; gridId<gridSize; gridId++) {
+    gridHash[gridId] = {};
+    gridHash[gridId]['red'] = null;
+    gridHash[gridId]['blue'] = null;
+  }
 }
 
 function createHTMLGrid(){
   for(let gridElemId=0; gridElemId<gridSize; gridElemId++){
-      var container = document.getElementsByClassName('container')[0];
-      console.log(container)
-      var newDivElem = document.createElement('div');
-      console.log(newDivElem);
-      container.appendChild(newDivElem);
-      newDivElem.setAttribute('id', gridElemId);
-      newDivElem.setAttribute('class', 'common');
+    var container = document.getElementsByClassName('container')[0];
+    var newDivElem = document.createElement('div');
+    container.appendChild(newDivElem);
+    newDivElem.setAttribute('id', gridElemId);
+    newDivElem.setAttribute('class', 'common');
   }    
 }
 
 function changeGridColors() {
-    var gridId = Math.floor(Math.random() * gridSize);
-    var gridTime = gridHash[gridId][gridColor];
-    var timeNow = Date.now();
-    var timeDifference = timeNow - gridTime;
-    if (timeDifference > timeChange || gridTime == null) {
-      var gridElem = document.getElementById(gridId);
-      gridElem.style.backgroundColor = gridColor;
-      gridColor = gridColor == 'red' ? 'blue' : 'red';
-      gridHash[gridId][gridColor] = timeNow;      
-    } 
+  var gridId = Math.floor(Math.random() * gridSize);
+  var gridTime = gridHash[gridId][gridColor];
+  var timeNow = Date.now();
+  var timeDifference = timeNow - gridTime;
+  if (timeDifference > timeChange || gridTime == null) {
+    var gridElem = document.getElementById(gridId);
+    gridElem.style.backgroundColor = gridColor;
+    gridColor = gridColor == 'red' ? 'blue' : 'red';
+    gridHash[gridId][gridColor] = timeNow;      
+  } 
 }
